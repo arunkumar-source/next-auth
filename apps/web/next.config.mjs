0bd5d21c38396519aee39bdf601063a9610bc3c9
arunkumar-source <arunkumar@webzenith.tech>
2026-02-14 01:10:49 +0530

    try 5

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -2,6 +2,10 @@
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
   // matcher:['/AddWorkKanban/:path*','/Dash/:path*']
+  env: {
+    DATABASE_URL: process.env.DATABASE_URL,
+    JWT_SECRET: process.env.JWT_SECRET,
+  },
 }
 
 export default nextConfig
